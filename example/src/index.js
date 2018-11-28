64d7a1d2bc00efcdad8a872903fb1671f0593834
Abdiel Brilhante <abdielbrilhante@gmail.com>
2018-11-28 14:16:15 -0300

    Update create-react-library structure

---
+++ example/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+import AppForm from './AppForm'
+
+const App = () => (
+  <AppForm />
+)
+
+render(<App />, document.getElementById('root'))
